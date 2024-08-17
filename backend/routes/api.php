49faec3c993e51d05b8e842ec02d53b6237320e2
robin-bosch <alphaclass@web.de>
2024-08-18 00:18:55 +0200

    Füge rate limit für die API hinzu

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,3 +5,12 @@ use App\Http\Controllers\KundeController;
 
 
 Route::apiResource('kunde', KundeController::class);
+
+// Rate limit für die API
+Route::middleware('throttle:60,1')->group(function () {
+    Route::get('/kunde', [KundeController::class, 'index']);
+    Route::post('/kunde', [KundeController::class, 'store']);
+    Route::get('/kunde/{id}', [KundeController::class, 'show']);
+    Route::put('/kunde/{id}', [KundeController::class, 'update']);
+    Route::delete('/kunde/{id}', [KundeController::class, 'destroy']);
+});
